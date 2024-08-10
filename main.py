3e1bdb1574e3aca5e8d3f284b76265fd99fcf3db
nadipetrovna <nadipetrovna@gmail.com>
2024-08-10 21:06:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# task1
+#1st program
+print(9.0**0.5*5)
+# task2
+#2nd program
+print (9.99 > 9.98 and 1000.0 !=1000.1)
+# task3
+#3rd program
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print (2 * (2 + 2) == 2 * 2 + 2)
+# task4
+#4th program
+print (int((float ('123.456')*10.0)%10))
